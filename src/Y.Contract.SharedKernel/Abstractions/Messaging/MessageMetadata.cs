ef01108686d002c6ae6e7aff77eb73a8135bcdf6
GuilhermeOM <guilhermeomalta0@gmail.com>
2026-01-23 22:36:56 -0300

    improvement: src projects organization

---
+++ src/Y.Contract.SharedKernel/Abstractions/Messaging/MessageMetadata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Y.Contract.SharedKernel.Abstractions.Messaging;
+public sealed record MessageMetadata
+{
+    public required string MessageKey { get; init; }
+    public required string Topic { get; init; }
+}
